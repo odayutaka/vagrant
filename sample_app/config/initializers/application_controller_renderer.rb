c7ad6aafbd32955c21cec1913331e5b47326930f
odayutaka <y.oda.sanwadock@gmail.com>
2020-04-09 17:20:22 +0900

    finish 3-9

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8794a16ab87898e69343381b7b4bcab82936baa5c5f8a3ffadb24af6a315574845fb919debdb3edacc7b20e10e0d02ba6c3adb5a0909569abde2f2f05e72841d'
