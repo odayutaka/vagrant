4f406daeb51f1d6281ec5214a3d5c452de718ea7
odayutaka <y.oda.sanwadock@gmail.com>
2020-04-17 11:35:37 +0900

    finish 5-3

--- meshiterro1/config/initializers/application_controller_renderer.rb
+++ meshiterro1/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3bab51901089e12538f28c742bd5ca417a0f4f3668975d99c0b6c3ed49b348fb12c1ba912f9be761aad6e444a22ab634383cfb0daf600fbbf75c9962b465db1f'
