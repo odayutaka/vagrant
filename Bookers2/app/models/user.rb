bfd44303fd656539277bba9e80f8e096293dfac8
odayutaka <y.oda.sanwadock@gmail.com>
2020-05-03 20:54:51 +0900

    finish task2

--- Bookers2/app/models/user.rb
+++ Bookers2/app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
   attachment :profile_image
-  validates :name, presence: true, length: { in: 2..20 }
+  validates :name, presence: true, length: {maximum: 20, minimum: 2}
   validates :introduction, length: { maximum: 50 }
 end
