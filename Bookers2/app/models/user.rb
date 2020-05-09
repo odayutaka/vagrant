3225a07308ea18a8f257564fa5eb937ef7235ce4
odayutaka <y.oda.sanwadock@gmail.com>
2020-05-09 16:54:30 +0900

    finish task3

--- Bookers2/app/models/user.rb
+++ Bookers2/app/models/user.rb
@@ -4,7 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_books, through: :favorites
+  has_many :book_comments, dependent: :destroy
   attachment :profile_image
   validates :name, presence: true, length: {maximum: 20, minimum: 2}
   validates :introduction, length: { maximum: 50 }
+  def favorited_by?(book_id)
+  	favorites.where(book_id: book_id).exists?
+  end
 end
--- Bookers2/config/routes.rb
+++ Bookers2/config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :books
   resources :users, only: [:index, :show, :edit, :update]
+  post 'favorite/:id' => 'favorites#create', as: 'create_favorite'
+  delete 'favorite/:id' => 'favorites#destroy', as: 'destroy_favorite'
+  resources :books do
+  	resources :book_comments, only: [:create, :destroy]
+  end
   get "home/about" => "home#about"
 end
